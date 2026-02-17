75e06a6371f93056dfdbd09b4fc37d0f8cf1ef6f
yusufhabs <yusufhabibullahs@gmail.com>
2026-02-17 13:25:14 +0700

    feat(seeding): add initial data seeding for peminjaman ruangan

---
+++ PeminjamanRuangan/Migrations/20260217062329_AddDataSeeding.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeminjamanRuangan.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
